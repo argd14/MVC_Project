3aa62b6337cecd28c14a53b6f0f9d9fa084622bb
Angel19098 <angelmelara44@gmail.com>
2022-02-03 10:59:44 -0600

    Arquitectura MVC

---
+++ src/main/java/Group05/MVC_Project/controllers/UserController.java
@@ -0,0 +1,7 @@
+package Group05.MVC_Project.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+}
---
+++ src/main/java/Group05/MVC_Project/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package Group05.MVC_Project.repositories;
+
+import Group05.MVC_Project.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
