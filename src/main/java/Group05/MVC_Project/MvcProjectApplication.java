9b210e748d6341da3947cd9cff0c8316afe78afe
Eduardo Ezequiel <57075508+eduardoezequieel@users.noreply.github.com>
2022-02-03 11:35:20 -0600

    Init commit

---
+++ src/main/java/Group05/MVC_Project/MvcProjectApplication.java
@@ -0,0 +1,13 @@
+package Group05.MVC_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcProjectApplication.class, args);
+	}
+
+}
