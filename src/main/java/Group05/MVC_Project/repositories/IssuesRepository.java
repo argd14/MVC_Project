3b2f7220f7efe852b60e8d3bce0b5ebc83a21054
Angel19098 <angelmelara44@gmail.com>
2022-02-09 19:49:00 -0600

    API user, register, validations, response, loggin, update, models, sprint

--- src/main/java/Group05/MVC_Project/repositories/IssuesRepository.java
+++ src/main/java/Group05/MVC_Project/repositories/IssuesRepository.java
@@ -4,6 +4,7 @@ package Group05.MVC_Project.repositories;
 import Group05.MVC_Project.models.Issue;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,5 +16,8 @@ public interface IssuesRepository extends JpaRepository<Issue, Long> {
             nativeQuery = true)
     List<Object> getAvailableIssues();
 
+    @Query(value = "SELECT c.id,c.summary FROM issue c WHERE c.id_development_cycle = :id",
+            nativeQuery = true)
+    List<Object> getAvailableIssuesBySprint(@Param("id") Long id);
 
 }
